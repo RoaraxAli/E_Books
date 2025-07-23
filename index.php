3014cf9ed20294166dc7df7cddcfda203a522f6d
Muhammd Ali <roaraxyt@gmail.com>
2025-07-23 05:21:37 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Welcome</title>
-</head>
-<body>
-    <h1>Welcome to My PHP App</h1>
-
-    <!-- Button that links to home page -->
-    <a href="user/pages/home.php">
-        <button>Go to Home Page</button>
-    </a>
-
-</body>
-</html>
+<?php
+header("Location: user/pages/home.php");
+exit();
+?>
