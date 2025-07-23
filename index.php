f13db11547a8af0ca4746b76e1e92a024bd12c5e
Muhammd Ali <roaraxyt@gmail.com>
2025-07-23 05:12:14 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-asdasdsadasd
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome to My PHP App</h1>
+
+    <!-- Button that links to home page -->
+    <a href="user/pages/home.php">
+        <button>Go to Home Page</button>
+    </a>
+
+</body>
+</html>
